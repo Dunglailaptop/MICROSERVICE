1f91d76793b5b3f191e004127afa3dcbc5113edb
dungctvippro <101032317+Dunglailaptop@users.noreply.github.com>
2024-06-16 14:47:49 +0700

    gitt

--- Services/AuthenticationService/LibraryAuthentication/LibraryAuthentication/Models/UsersServiceContext.cs
+++ Services/AuthenticationService/LibraryAuthentication/LibraryAuthentication/Models/UsersServiceContext.cs
@@ -28,7 +28,7 @@ public partial class UsersServiceContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;database=users_service;user id=root;password=2792001dung", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.36-mysql"));
+        => optionsBuilder.UseMySql("server=127.0.0.1;database=users_service;user id=root;password=2792001dung", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.36-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
